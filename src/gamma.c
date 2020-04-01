ab45f28222f44c1bd09d39d034b2a875b6963024
wdrz <wtddrz@gmail.com>
2020-04-01 20:51:12 +0200

    Napisane od nowa, drugie podejscie nadal bez golden_move

--- src/gamma.c
+++ src/gamma.c
@@ -8,26 +8,31 @@ int X[4] = {1, -1, 0, 0};
 int Y[4] = {0, 0, 1, -1};
 
 struct gamma {
-  uint32_t *board, *player_fields, *player_areas, *player_adjacent;
-  uint32_t players, width, height, areas;
+  uint64_t *board;
+  uint32_t *player_fields, *player_areas, *player_adjacent;
+  uint32_t players, width, height, max_areas;
   uint64_t empty_fields;
   bool *player_golden_used;
 };
 
 gamma_t* gamma_new(uint32_t width, uint32_t height,
   uint32_t players, uint32_t areas) {
-    // TODO: powinien sie wywalac jak niepoprawne argumenty albo nie da sie zaalokowac
+    // TODO: powinien sie wywalac jak niepoprawne argumenty
     gamma_t *g = malloc (sizeof(struct gamma));
+    if (g == NULL) exit (1);
     g -> players = players;
     g -> width = width;
     g -> height = height;
-    g -> areas = areas;
+    g -> max_areas = areas;
     g -> empty_fields = width * height;
-    g -> board = calloc (0, sizeof (uint32_t) * width * height);
-    g -> player_fields = calloc (0, sizeof (uint32_t) * players);
-    g -> player_areas = calloc (0, sizeof (uint32_t) * players);
-    g -> player_adjacent = calloc (0, sizeof (uint32_t) * players);
-    g -> player_golden_used = calloc (false, sizeof (bool) * players);
+    g -> board = calloc (width * height, sizeof (uint32_t) + 1);
+    g -> player_fields = calloc (players, sizeof (uint32_t));
+    g -> player_areas = calloc (players, sizeof (uint32_t));
+    g -> player_adjacent = calloc (players, sizeof (uint32_t));
+    g -> player_golden_used = calloc (players, sizeof (bool));
+    if (g -> board == NULL || g -> player_areas == NULL
+    || g -> player_adjacent == NULL || g -> player_golden_used == NULL)
+      exit (1);
     return g;
 }
 
@@ -40,61 +45,99 @@ void gamma_delete(gamma_t *g) {
   free (g);
 }
 
-/* Zwraca wskaznik na fragment pamieci przechowujacej dane dla pola x, y
-   jesli nie ma takiego pola, zwraca NULL */
-uint32_t* get_field (gamma_t *g, uint32_t x, uint32_t y) {
+/* Zwraca pozycje pola zadanego przez wspolrzedne. Pozycja jest to adres
+   w pamieci pola, pomniejszony o adres poczatku tablicy z polami. */
+uint64_t get_field (gamma_t *g, uint32_t x, uint32_t y) {
   if (x < g -> width && y < g -> height)
-    return (g -> board + (g -> height * y) + x);
+    return (g -> height * y) + x + 1;
   else
-    return NULL;
+    return 0;
 }
 
-bool field_eq (uint32_t* f, uint32_t value) {
-  if (f == NULL) return false;
-  return (*f) == value;
+/* Zwraca pozycje reprezentatnta pola. Pozycja pola zostala zdefiniowana wyzej. */
+uint64_t find (gamma_t *g, uint64_t position) {
+  uint64_t* zawartosc = g -> board + position;
+  if ( *zawartosc >=  (- g -> players) || *zawartosc == 0) return position;
+  *zawartosc = find(g, *zawartosc);
+  return *zawartosc;
 }
 
-/* Sprawdza, czy pole sasiaduje z polem gracza */
-bool is_field_adjacent (gamma_t *g, uint32_t x, uint32_t y, uint32_t player) {
-  if (field_eq(get_field(g, x + 1, y), player) ||
-      field_eq(get_field(g, x - 1, y), player) ||
-      field_eq(get_field(g, x, y + 1), player) ||
-      field_eq(get_field(g, x, y - 1), player) )
-    return true;
-  else
-    return false;
+/* Laczy dwa obszary o ile naleza one do tego samego gracza.
+   Oczekuje, ze podane zostana pozycje reprezentantow dwoch obszarow.
+   Moga byc to te same obszary */
+void union_g (gamma_t *g, uint64_t pos_rep1, uint64_t pos_rep2) {
+  if (pos_rep1 == 0 || pos_rep2 == 0) return;
+  if (g -> board[pos_rep1] != g -> board[pos_rep2]) return;
+  g -> board[pos_rep1] = pos_rep2;
+}
+
+/* Jesli pole (x, y) nie sasiaduje z polem gracza zwraca 0.
+   W przeciwnym przypadku zwraca reprezentanta jednego z nich */
+uint64_t is_adjacent (gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
+  uint32_t i;
+  for (i = 0; i < 4; i++)
+    if (g -> board[find(g, get_field(g, x + X[i], y + Y[i]))] == player)
+      return find(g, get_field(g, x + X[i], y + Y[i]));
+  return false;
 }
 
 bool gamma_move (gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
+  uint64_t pos = get_field(g, x, y);
   uint32_t i;
-  if (player == 0 || g -> players < player)
-    return false;
-  uint32_t *f = get_field(g, x, y);
-  if (f == NULL) return false;
-  if (*f != 0) return false;
+  // Czy dane wejsciowe sa poprawne? (TODO: sprawdzic playera, plansze)
+  // Czy wskazano puste pole wewnatrz planszy?
+  if (pos == 0 || g -> board[pos] != 0) return false;
 
-  if (is_field_adjacent(g, x, y, player)) {
-    g -> player_adjacent[player - 1] --;
-  } else {
-    if (g -> player_areas[player - 1] >= g -> areas) {
-      return false;
-    }
-    g -> player_areas[player - 1] ++;
-  }
+  // Ruch nie moze zwiekszyc liczby obszarow gracza powyzej [max_areas]
+  uint64_t adj = is_adjacent(g, player, x, y);
+  if (g -> player_areas[player - 1] == g -> max_areas && adj == 0)
+    return false;
+
+  // Zbadajmy jacy sa reprezentanci sasiadow pola (x, y)
+  uint64_t hood[4]; // pozycje reprezentatow pol sasiadujacych z (x, y)
+  uint32_t wolne; // liczba pustych pol sasiadujacych z (x, y) ktore nie sasiaduja z polami gracza player
   for (i = 0; i < 4; i++) {
-    if (! is_field_adjacent(g, x + X[i], y + Y[i], player) &&
-          field_eq(get_field(g, x + X[i], y + Y[i]), 0)) {
-      g -> player_adjacent[player - 1] ++;
-    }
+    hood[i] = find(g, get_field(g, x + X[i], y + Y[i]));
   }
-  g -> empty_fields --;
-  g -> player_fields[player - 1] ++;
-  *f = player;
+  // Postawinie pionka byc moze zmienilo troszke liczbe pol na ktorych gracz
+  // moze sie ruszyc bez zwiekszania liczby swoich obszarow.
+  for (i = 0; i < 4; i++)
+    if (! is_adjacent(g, player, x + X[i], y + Y[i]))
+      g -> player_adjacent[player - 1]++;
+  if (adj != 0) g -> player_adjacent[player - 1]--;
+
+  // Byc moze nowe pole jest mostem laczacym 2, 3, a moze nawet 4 obszary gracza.
+  // Sprawdzmy, czy tak jest i polaczmy je ze soba jesli trzeba!
+  if (adj != 0)
+    for (i = 0; i < 4; i++)
+      union_g (g, adj, hood[i]);
+
+  // Miejsca na ktorych inni gracze moga postawic pionka bez zwiekszania liczby
+  // swoich obszarow tez wlasnie sie zmienila. Uwzglednijmy to w wyliczniach!
+  for (i = 0; i < 4; i++)
+    if (g -> board[hood[i]] != 0 && g -> board[hood[i]] != player)
+      g -> player_adjacent[ g -> board[hood[i]] ]--;
+
+  // Zmienila sie liczba obszarow gracza? Liczba pol na pewno. Tak samo liczba
+  // pustych pol na plaszy. Nadpiszmy tez w koncu pole o ktore sie rozchodzi
+  if (adj == 0) {
+    g -> player_areas[player - 1]++;
+    g -> board[pos] = player;
+  } else {
+    g -> board[pos] = adj;
+  }
+  g -> player_fields[player - 1]++;
+  g -> empty_fields--;
   return true;
 }
 
 bool gamma_golden_move(gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
-  // TODO !!!!!!!!!
+  // TODO: Sprawdzic poprawnosc danych
+
+  // Sprawdzamy, czy pole nalezy do innego gracza
+  uint64_t pos = get_field(g, x, y);
+  if (find(g, pos)g -> board[])
+
   return false;
 }
 
@@ -103,7 +146,7 @@ uint64_t gamma_busy_fields(gamma_t *g, uint32_t player) {
 }
 
 uint64_t gamma_free_fields(gamma_t *g, uint32_t player) {
-  if (g -> player_areas[player - 1] < g -> areas)
+  if (g -> player_areas[player - 1] < g -> max_areas)
     return g -> empty_fields;
   else
     return g -> player_adjacent[player - 1];
@@ -121,14 +164,10 @@ char* gamma_board(gamma_t *g) {
   uint32_t i, j;
   for (i = 0; i < g -> width; i++) {
     for (j = 0; j < g -> height; j++) {
-      board[(g -> width + 1) * j + i] = (char) '0' - 1 + *get_field(g, i, j);
+      board[(g -> width + 1) * j + i] = (char) '0' - 1 + g -> board[get_field(g, i, j)];
     }
     board[(g -> width + 1) * j + g -> width] = '\n';
   }
   board[(g -> width + 1) * g-> height] = '\0';
   return board;
-}
-
-int main (void) {
-  return 0;
 }
